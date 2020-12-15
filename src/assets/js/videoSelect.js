8e020ce194eaca84b899cc4e20e23a3336063589
ferraz <afa00.papa@gmail.com>
2020-12-15 18:00:34 -0300

    Atualizações

--- src/assets/js/videoSelect.js
+++ src/assets/js/videoSelect.js
@@ -1,13 +1,19 @@
 const itens = document.querySelectorAll('.conteudo-video__lista li .conteudo-video__informacoes');
 const videoPrincipal = document.querySelector('.video-principal');
+const elementoAtivoInicial = document.getElementsByClassName("ativo");
 
 itens.forEach((element)=>{
-    element.addEventListener('click', SelectURL);
+    element.addEventListener('click', SelectVideo);
 });
 
-function SelectURL(element){
-    const div = element.target;
+
+function SelectVideo(element){
+    const div = element.target.parentElement;
     const videoURL = div.getAttribute('data-src');
+
+    //Adicionar class 'ativo' ao click
+    elementoAtivoInicial[0].classList.remove('ativo');
+    div.classList.add('ativo');
 
     return videoPrincipal.src = videoURL;
 };
