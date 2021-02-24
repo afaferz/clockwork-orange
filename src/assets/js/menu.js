8a890d214551d01a2af4292ea3f153de568535bb
afaferz <afa00.papa@gmail.com>
2021-02-24 04:18:48 -0300

    Update

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,9 +1,17 @@
 const botaoMenu = document.getElementById('menu-toggle');
 const sidebar = document.querySelector('.sidebar');
 
-botaoMenu.addEventListener('click', sidebarmenu)
+// botaoMenu.addEventListener('click', sidebarmenu)
 
 function sidebarmenu(){
     sidebar.classList.toggle('ativo');
     botaoMenu.classList.toggle('ativo');
 }
+
+document.documentElement.addEventListener('click', (e)=>{
+  sidebarmenu();
+  if(e.target != botaoMenu){
+    sidebar.classList.remove('ativo');
+    botaoMenu.classList.remove('ativo');
+  } 
+});
